3ecf3c9601d6ddec39621a61d76fd0d22d5bc3a7
soenneker <jake@soenneker.com>
2026-08-29 14:17:29 -0500

    Improve public API XML documentation

--- src/Soenneker.ZipCodes.Runners.GeoNames/Utils/Abstract/IFileOperationsUtil.cs
+++ src/Soenneker.ZipCodes.Runners.GeoNames/Utils/Abstract/IFileOperationsUtil.cs
@@ -11,8 +11,8 @@ public interface IFileOperationsUtil
     /// <summary>
     /// Builds zip code geometry file.
     /// </summary>
-    /// <param name="zipFilePath">The zip file path.</param>
-    /// <param name="cancellationToken">The cancellation token.</param>
-    /// <returns>A task containing the result of the operation.</returns>
+    /// <param name="zipFilePath">Path of the zip file to use.</param>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the text returned by build Zip Code Geometry File.</returns>
     ValueTask<string> BuildZipCodeGeometryFile(string zipFilePath, CancellationToken cancellationToken = default);
 }
